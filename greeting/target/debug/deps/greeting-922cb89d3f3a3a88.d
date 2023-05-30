8257b66c6056f61143ca063d66f0ae1ca9c790b5
yufan <yufan@huaqin.com>
2023-05-30 19:30:31 +0800

    changed git

--- greeting/target/debug/deps/greeting-922cb89d3f3a3a88.d
+++
@@ -1,5 +0,0 @@
-E:\Developments\Rust\runoob-greeting\Rust_Learn\greeting\target\debug\deps\greeting-922cb89d3f3a3a88.exe: src\main.rs
-
-E:\Developments\Rust\runoob-greeting\Rust_Learn\greeting\target\debug\deps\greeting-922cb89d3f3a3a88.d: src\main.rs
-
-src\main.rs:
--- greeting/target/debug/greeting.d
+++
@@ -1,1 +0,0 @@
-E:\Developments\Rust\runoob-greeting\Rust_Learn\greeting\target\debug\greeting.exe: E:\Developments\Rust\runoob-greeting\Rust_Learn\greeting\src\main.rs
